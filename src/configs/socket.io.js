565ebd6144485e541a4faec2b320485e3efb674b
Andrey Checha <chechaandrey23@gmail.com>
2023-02-05 08:45:00 +0200

    fix URL

--- src/configs/socket.io.js
+++ src/configs/socket.io.js
@@ -1,3 +1,4 @@
 import io from 'socket.io-client';
 
-export const socket = io.connect('http://localhost:3300');
+//export const socket = io.connect('http://localhost:3300');
+export const socket = io.connect('https://hidden-lake-90711.herokuapp.com/');
